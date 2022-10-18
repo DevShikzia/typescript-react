c4038588ee191296e94d3c4c0c1ef2c7c7ab39ff
DevShikzia <devsanchezmatias@gmail.com>
2022-10-18 13:56:49 -0300

    formlist added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
+import Form from './components/Form';
+import List from './components/List';
+import {Sub} from './types'
 
+interface appState{
+  subs: Sub[];
+}
+
+const INITIAL_STATE = [
+  {
+    nick: 'Illio',
+    subMonths: 3,
+    avatar:'https://i.pravatar.cc/150?u=Illio',
+    description: 'Illio es vip'
+  },
+  {
+    nick: 'lucio_fernandez',
+    subMonths:5,
+    avatar: 'https://i.pravatar.cc/150?u=lucio_fernandez',
+
+  },
+]
 function App() {
+  const [subs, setSubs] = useState<appState["subs"]>([])
+  const divRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    setSubs(INITIAL_STATE)
+  },[])
+
+  const handleNewSubs = (newSub: Sub): void => {
+    setSubs(subs => [...subs, newSub])
+  } 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" ref={divRef}>
+      <h1>Shikzia Subs</h1>
+      <List subs={subs} />
+      <Form onNewSub={handleNewSubs}/>
     </div>
   );
 }
